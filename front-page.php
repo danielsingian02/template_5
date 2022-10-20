ccd37838738edeb133d5241961f75b9115ef916d
Daniel Singian <daniel.singian02@gmail.com>
2022-10-20 16:44:50 +0800

    Add CSS to the Header, Frontpage and Footer

--- front-page.php
+++ front-page.php
@@ -1,24 +1,36 @@
-<div class="banner"><img src="<?php echo get_theme_mod('banner_image',get_bloginfo('template_directory').'/assets/images/Asset1.png' )?>" alt=""></div>
-    <div class="heading">
-        <div class="left-text">
-            <h1><?php printf( get_theme_mod('second_section_heading', __( 'Welcome To Our Home', 'second_section_heading' ) )); ?></h1>
-            <p><?php printf( get_theme_mod('second_section_paragraph', __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum nunc turpis arcu dui cursus habitasse elit tempus nisi. Id nunc feugiat id vel sollicitudin aenean. Volutpat, id viverra elit nisi. Pharetra, quisque enim neque justo, ac venenatis in vitae. Eleifend elementum quis accumsan risus dictum ornare augue at eu. Accumsan, faucibus morbi amet nunc. Tincidunt sed nulla tincidunt sit eu.', 'second_section_paragraph' ) )); ?></p>
-        </div>
-        <div class="right-image">
-            <div class="img1 pair"><img src="<?php echo get_theme_mod('top_image',get_bloginfo('template_directory').'/assets/images/Asset2.png' )?>" alt=""></div>
-            <div class="img2 pair"><img src="<?php echo get_theme_mod('bottom_image',get_bloginfo('template_directory').'/assets/images/Asset3.png' )?>" alt=""></div>
-        </div>
-    </div>
-    <div class="listitems">
-        <button><a class= "description_link" href="<?php echo get_theme_mod('banner_button_url', __( '#', 'second_section_button_url' ) ); ?>"><?php printf( get_theme_mod('second_section_button_text', __( 'News & Announcements ', 'second_section_button_text' ) )); ?></a></button>
-        <button><a class= "description_link" href="<?php echo get_theme_mod('banner_button_url2', __( '#', 'second_section_button_url2' ) ); ?>"><?php printf( get_theme_mod('second_section_button_text2', __( 'Upcoming Events  ', 'second_section_button_text2' ) )); ?></a></button>
-        <button><a class= "description_link" href="<?php echo get_theme_mod('banner_button_url3', __( '#', 'second_section_button_url3' ) ); ?>"><?php printf( get_theme_mod('second_section_button_text3', __( ' Board Meeting Notice & Agenda ', 'second_section_button_text3' ) )); ?></a></button>
-        <button><a class= "description_link" href="<?php echo get_theme_mod('banner_button_url4', __( '#', 'second_section_button_url4' ) ); ?>"><?php printf( get_theme_mod('second_section_button_text4', __( ' Member Announcements  ', 'second_section_button_text4' ) )); ?></a></button>
-        <button><a class= "description_link" href="<?php echo get_theme_mod('banner_button_url5', __( '#', 'second_section_button_url5' ) ); ?>"><?php printf( get_theme_mod('second_section_button_text5', __( ' Site Search  ', 'second_section_button_text5' ) )); ?></a></button>
-        <button><a class= "description_link" href="<?php echo get_theme_mod('banner_button_url6', __( '#', 'second_section_button_url6' ) ); ?>"><?php printf( get_theme_mod('second_section_button_text6', __( ' Documents  ', 'second_section_button_text6' ) )); ?></a></button>
-    </div>
-    <div class="map"><img src="<?php echo get_theme_mod('map',get_bloginfo('template_directory').'/assets/images/Asset4.png' )?>" alt=""></div>
-    <div class="we">
-        <h1><?php printf( get_theme_mod('third_heading', __( 'We are One', 'third_heading' ) )); ?></h1>
-        <p><?php printf( get_theme_mod('third_description', __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum nunc turpis arcu dui cursus habitasse elit tempus nisi. Id nunc feugiat id vel sollicitudin aenean. Volutpat, id viverra elit nisi. Pharetra, quisque enim neque justo, ac venenatis in', 'third_description' ) )); ?></p>
-    </div>	
+<?php
+get_header();
+?>
+<body>
+	<div class="banner">
+		<img src="<?php echo get_theme_mod( 'banner_image', get_bloginfo( 'template_directory' ) . '/assets/images/Asset1.png' ); ?>" alt="">
+	</div>
+	<div class="heading">
+		<div class="left-text">
+			<h1><?php printf( get_theme_mod( 'second_section_heading', __( 'Welcome To Our Home', 'second_section_heading' ) ) ); ?></h1>
+			<p><?php printf( get_theme_mod( 'second_section_paragraph', __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum nunc turpis arcu dui cursus habitasse elit tempus nisi. Id nunc feugiat id vel sollicitudin aenean. Volutpat, id viverra elit nisi. Pharetra, quisque enim neque justo, ac venenatis in vitae. Eleifend elementum quis accumsan risus dictum ornare augue at eu. Accumsan, faucibus morbi amet nunc. Tincidunt sed nulla tincidunt sit eu.', 'second_section_paragraph' ) ) ); ?></p>
+		</div>
+		<div class="right-image">
+			<div class="img1-pair"><img src="<?php echo get_theme_mod( 'top_image', get_bloginfo( 'template_directory' ) . '/assets/images/Asset2.png' ); ?>" alt=""></div>
+			<div class="img2-pair"><img src="<?php echo get_theme_mod( 'bottom_image', get_bloginfo( 'template_directory' ) . '/assets/images/Asset3.png' ); ?>" alt=""></div>
+		</div>
+	</div>
+	<div class="listitems">
+		<button><a class= "description_link" href="<?php echo get_theme_mod( 'banner_button_url', __( '#', 'second_section_button_url' ) ); ?>"><?php printf( get_theme_mod( 'second_section_button_text', __( 'News & Announcements ', 'second_section_button_text' ) ) ); ?></a></button>
+		<button><a class= "description_link" href="<?php echo get_theme_mod( 'banner_button_url2', __( '#', 'second_section_button_url2' ) ); ?>"><?php printf( get_theme_mod( 'second_section_button_text2', __( 'Upcoming Events  ', 'second_section_button_text2' ) ) ); ?></a></button>
+		<button><a class= "description_link" href="<?php echo get_theme_mod( 'banner_button_url3', __( '#', 'second_section_button_url3' ) ); ?>"><?php printf( get_theme_mod( 'second_section_button_text3', __( ' Board Meeting Notice & Agenda ', 'second_section_button_text3' ) ) ); ?></a></button>
+		<button><a class= "description_link" href="<?php echo get_theme_mod( 'banner_button_url4', __( '#', 'second_section_button_url4' ) ); ?>"><?php printf( get_theme_mod( 'second_section_button_text4', __( ' Member Announcements  ', 'second_section_button_text4' ) ) ); ?></a></button>
+		<button><a class= "description_link" href="<?php echo get_theme_mod( 'banner_button_url5', __( '#', 'second_section_button_url5' ) ); ?>"><?php printf( get_theme_mod( 'second_section_button_text5', __( ' Site Search  ', 'second_section_button_text5' ) ) ); ?></a></button>
+		<button><a class= "description_link" href="<?php echo get_theme_mod( 'banner_button_url6', __( '#', 'second_section_button_url6' ) ); ?>"><?php printf( get_theme_mod( 'second_section_button_text6', __( ' Documents  ', 'second_section_button_text6' ) ) ); ?></a></button>
+	</div>
+	<div class="map">
+		<img src="<?php echo get_theme_mod( 'map', get_bloginfo( 'template_directory' ) . '/assets/images/Asset4.png' ); ?>" alt="">
+	</div>
+	<div class="we">
+		<h1><?php printf( get_theme_mod( 'third_heading', __( 'We are One', 'third_heading' ) ) ); ?></h1>
+		<p><?php printf( get_theme_mod( 'third_description', __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum nunc turpis arcu dui cursus habitasse elit tempus nisi. Id nunc feugiat id vel sollicitudin aenean. Volutpat, id viverra elit nisi. Pharetra, quisque enim neque justo, ac venenatis in', 'third_description' ) ) ); ?></p>
+	</div>
+</body>
+<?php
+get_footer();
+?>
